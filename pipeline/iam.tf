20fa448cc321b19a186913817ba0733c3ec7bfd8
Ray <example@example.com>
2019-06-20 06:50:07 +0900

    star34

--- pipeline/iam.tf
+++ pipeline/iam.tf
@@ -159,7 +159,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
     },
     {
       "Effect": "Allow",
-      "Action": "cloudfront:*"
+      "Action": "cloudfront:*",
       "Resource": "*"
     }
   ]
