2148d867320d7ba9369f95cbafc356cf885f2b53
Ray <example@example.com>
2019-06-24 03:46:28 +0900

    start5

--- lambda_edge.js
+++ lambda_edge.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
 	    const response = {
 			        status: '200',
 			        statusDescription: 'OK',
-			        body: "Lambda@Edge is awesome! Ver.3",
+			        body: "Lambda@Edge is awesome! Ver.4",
 			    };
 	    callback(null, response);
 };
