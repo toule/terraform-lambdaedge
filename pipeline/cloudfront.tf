bac2623874ecfae95c5aa7983fcdc6101de4e240
Ray <example@example.com>
2019-06-25 05:41:35 +0900

    start20

--- pipeline/cloudfront.tf
+++ pipeline/cloudfront.tf
@@ -44,3 +44,32 @@ resource "aws_cloudfront_distribution" "edge_distribution" {
 	}
 }
 
+output "cf_id" {
+  value       = "${aws_cloudfront_distribution.edge_distribution.id}"
+  description = "ID of AWS CloudFront distribution"
+}
+
+output "cf_arn" {
+  value       = "${aws_cloudfront_distribution.edge_distribution.arn}"
+  description = "ARN of AWS CloudFront distribution"
+}
+
+output "cf_status" {
+  value       = "${aws_cloudfront_distribution.edge_distribution.status}"
+  description = "Current status of the distribution"
+}
+
+output "cf_domain_name" {
+  value       = "${aws_cloudfront_distribution.edge_distribution.domain_name}"
+  description = "Domain name corresponding to the distribution"
+}
+
+output "cf_etag" {
+  value       = "${aws_cloudfront_distribution.edge_distribution.etag}"
+  description = "Current version of the distribution's information"
+}
+
+output "cf_hosted_zone_id" {
+  value       = "${aws_cloudfront_distribution.edge_distribution.hosted_zone_id}"
+  description = "CloudFront Route 53 zone ID"
+}
