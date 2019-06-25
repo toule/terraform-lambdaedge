69ae0b9ba3823bf71ab50623fa2b4e23dfc7319f
Ray <example@example.com>
2019-06-25 10:51:45 +0900

    test start 1

--- lambda_edge.js
+++ lambda_edge.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
 	    const response = {
 			        status: '200',
 			        statusDescription: 'OK',
-			        body: "Lambda@Edge is awesome! Ver.6",
+			        body: "Welcome to CloudFront Day Ver.1 - Rayhli",
 			    };
 	    callback(null, response);
 };
--- pipeline/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "bucket-tf" {
-	bucket = "terraform-ray-infra-tf-us-east-1"
-	acl = "private"
-
-	tags = {
-		Name = "source/build Arti"
-		Environment = "DEV"
-	}
-}
