fb690def0605ffbe9b505ea1ddc1124416def719
Ray <example@example.com>
2019-06-25 14:44:20 +0900

    test start 19

--- lambda_edge.js
+++ lambda_edge.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
 	    const response = {
 			        status: '200',
 			        statusDescription: 'OK',
-			        body: "Welcome to CloudFront Day Ver.1 - Rayhli",
+			        body: "Welcome to CloudFront Day - Rayhli",
 			    };
 	    callback(null, response);
 };
